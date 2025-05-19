26ab2d76892120834b862eaf35081e4732f13979
Ignacio Zúñiga Navarro <nnznn-@hotmail.com>
2025-05-19 18:00:26 -0300

    Fix vite base path for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: './',
 })
